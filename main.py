db4fc52291fc0236acc528c4c6a1374ab9ce15aa
Vít Kuntoš <v.kuntos@gmail.com>
2022-01-09 17:24:26 +0100

    app

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory # filedialog - otevirá a ukládá funkce dialogu, askdirectory - popup s nastavením cesty
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+musicplayer = tkr.Tk()
+musicplayer.title("Music Player")
+musicplayer.geometry("450x350")
+
+directory = askdirectory()
+os.chdir(directory) # změní aktuální pracovní adresář na zadanou cestu. Jako cestu k novému adresáři přijímá pouze jediný argument
+songlist = os.listdir() # seznam obsahující názvy položek v adresáři zadaném cestou
+playlist = tkr.Listbox(musicplayer, font="Arial 12", bg="#C0C0C0", selectmode=tkr.SINGLE) # zobrazení textového seznamu uživateli a vybrání jednoho songu k přehrání
+
+for item in songlist:
+    pos = 0
+    playlist.insert(pos, item)
+    pos += 1
+
+pygame.init()
+pygame.mixer.init() # po inicializaci nahraje a pustí hudbu
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def play():
+    pygame.mixer.music.load(playlist.get(tkr.ACTIVE)) #modlul pro kontrolu audia, nahraje hudbu
+    var.set(playlist.get(tkr.ACTIVE))
+    pygame.mixer.music.play()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def stop():
+    pygame.mixer.music.stop()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def pause():
+    pygame.mixer.music.pause()
+
+
+def unpause():
+    pygame.mixer.music.unpause()
+
+
+Button1 = tkr.Button(musicplayer, width=5, height=2, font="Helvetica 13 bold", text="PLAY", command=play, bg="#33cc33",
+                     fg="white")
+Button2 = tkr.Button(musicplayer, width=5, height=2, font="Helvetica 13 bold", text="STOP", command=stop,
+                     bg="#ff6600", fg="white")
+Button3 = tkr.Button(musicplayer, width=5, height=2, font="Helvetica 13 bold", text="PAUSE", command=pause, bg="#ffa366",
+                     fg="white")
+Button4 = tkr.Button(musicplayer, width=5, height=2, font="Helvetica 13 bold", text="UNPAUSE", command=unpause,
+                     bg="#85e085", fg="white")
+
+var = tkr.StringVar()
+songtitle = tkr.Label(musicplayer, font="Helvetica 14 bold", bg="#ffffcc", textvariable=var)
+
+songtitle.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+playlist.pack(fill="both", expand="yes")
+
+musicplayer.mainloop()
